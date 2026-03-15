5bcf2e0edb984b9ff195d57ff882a9d3ea4dcb5e
Fliits <77997878+Fliits@users.noreply.github.com>
2026-03-15 20:21:48 +0200

    Update Database.java

--- src/main/java/simulation/Database.java
+++ src/main/java/simulation/Database.java
@@ -2,6 +2,9 @@ package simulation;
 
 import java.sql.*;
 
+/**
+ * Database class handles the connection to the database and provides methods for saving customers, retrieving service analysis, and deleting the database.
+ */
 public class Database {
     private static Connection conn;
 
